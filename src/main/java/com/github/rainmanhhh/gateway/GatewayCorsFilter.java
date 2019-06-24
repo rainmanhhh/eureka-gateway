1502f8e47e54c16c80d4bac6969f8bf2bac2d528
huangwl <@qq.com>
2019-06-24 11:48:09 +0800

    improve cors handling

---
+++ src/main/java/com/github/rainmanhhh/gateway/GatewayCorsFilter.java
@@ -0,0 +1,76 @@
+package com.github.rainmanhhh.gateway;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class GatewayCorsFilter implements WebFilter {
+    private Set<String> allowedOrigins = new HashSet<>(0);
+    private String allowedHeaders = null;
+    private String allowedMethods = null;
+    private String allowCredentials = "false";
+    private String exposedHeaders = null;
+    private String maxAge = null;
+
+    public GatewayCorsFilter(GatewayProps gatewayProps) {
+        CorsConfiguration corsProps = gatewayProps.getCors();
+        if (corsProps == null) corsProps = new CorsConfiguration().applyPermitDefaultValues();
+        List<String> allowedOrigins = corsProps.getAllowedOrigins();
+        if (allowedOrigins != null) this.allowedOrigins.addAll(allowedOrigins);
+        List<String> allowedHeaders = corsProps.getAllowedHeaders();
+        if (allowedHeaders != null) this.allowedHeaders = String.join(",", allowedHeaders);
+        List<String> allowedMethods = corsProps.getAllowedMethods();
+        if (allowedMethods != null) this.allowedMethods = String.join(",", allowedMethods);
+        Boolean allowCredentials = corsProps.getAllowCredentials();
+        if (allowCredentials != null) this.allowCredentials = allowCredentials.toString();
+        List<String> exposedHeaders = corsProps.getExposedHeaders();
+        if (exposedHeaders != null) this.exposedHeaders = String.join(",", exposedHeaders);
+        Long maxAge = corsProps.getMaxAge();
+        if (maxAge != null) this.maxAge = maxAge.toString();
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        HttpHeaders responseHeaders = response.getHeaders();
+        if (request.getMethod() == HttpMethod.OPTIONS) {
+            response.setStatusCode(HttpStatus.OK);
+            responseHeaders.set(HttpHeaders.ALLOW, "*");
+            return Mono.empty();
+        } else {
+            if (CorsUtils.isCorsRequest(request)) {
+                String requestOrigin = request.getHeaders().getOrigin();
+                if (allowedOrigins.contains("*") || allowedOrigins.contains(requestOrigin)) {
+                    responseHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, requestOrigin);
+                    if (allowedHeaders != null)
+                        responseHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, allowedHeaders);
+                    if (allowedMethods != null)
+                        responseHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, allowedMethods);
+                    if (allowCredentials != null)
+                        responseHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, allowCredentials);
+                    if (exposedHeaders != null)
+                        responseHeaders.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, exposedHeaders);
+                    if (maxAge != null)
+                        responseHeaders.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, maxAge);
+                }
+            }
+            return chain.filter(exchange);
+        }
+    }
+}
