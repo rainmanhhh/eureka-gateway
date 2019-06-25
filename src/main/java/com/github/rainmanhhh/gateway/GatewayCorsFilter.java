0e9e2f2e0b84936ea97f253e05065cc39ae3cfcd
huangwl <@qq.com>
2019-06-25 20:22:19 +0800

    fix bug: GatewayCorsFilter applied twice when accessing from localhost

--- src/main/java/com/github/rainmanhhh/gateway/GatewayCorsFilter.java
+++ src/main/java/com/github/rainmanhhh/gateway/GatewayCorsFilter.java
@@ -10,7 +10,6 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -34,7 +33,8 @@ public class GatewayCorsFilter implements WebFilter {
     public GatewayCorsFilter(GatewayProps gatewayProps, Environment environment) {
         Set<String> activeProfiles = Set.of(environment.getActiveProfiles());
         CorsConfiguration corsProps = gatewayProps.getCors();
-        if (corsProps == null) corsProps = new CorsConfiguration().applyPermitDefaultValues();
+        if (corsProps == null) corsProps = new CorsConfiguration();
+        corsProps.applyPermitDefaultValues();
         List<String> allowedOrigins = corsProps.getAllowedOrigins();
         if (allowedOrigins != null) {
             if ((activeProfiles.contains("production") || activeProfiles.contains("prod"))
@@ -56,17 +56,21 @@ public class GatewayCorsFilter implements WebFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        ServerHttpResponse response = exchange.getResponse();
-        HttpHeaders responseHeaders = response.getHeaders();
-        if (request.getMethod() == HttpMethod.OPTIONS) {
-            response.setStatusCode(HttpStatus.OK);
-            responseHeaders.set(HttpHeaders.ALLOW, "*");
-            return Mono.empty();
-        } else {
-            if (CorsUtils.isCorsRequest(request)) {
+        Object flag = exchange.getAttribute(getClass().getName());
+        if (flag == null) {
+            exchange.getAttributes().put(getClass().getName(), true);
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            HttpHeaders responseHeaders = response.getHeaders();
+            if (request.getMethod() == HttpMethod.OPTIONS) {
+                response.setStatusCode(HttpStatus.OK);
+                responseHeaders.set(HttpHeaders.ALLOW, "*");
+                return Mono.empty();
+            } else {
                 String requestOrigin = request.getHeaders().getOrigin();
-                if (allowedOrigins.contains("*") || allowedOrigins.contains(requestOrigin)) {
+                if (requestOrigin != null &&
+                        (allowedOrigins.contains("*") || allowedOrigins.contains(requestOrigin))
+                ) {
                     responseHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, requestOrigin);
                     if (allowedHeaders != null)
                         responseHeaders.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, allowedHeaders);
@@ -79,8 +83,8 @@ public class GatewayCorsFilter implements WebFilter {
                     if (maxAge != null)
                         responseHeaders.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, maxAge);
                 }
+                return chain.filter(exchange);
             }
-            return chain.filter(exchange);
-        }
+        } else return chain.filter(exchange);
     }
 }
