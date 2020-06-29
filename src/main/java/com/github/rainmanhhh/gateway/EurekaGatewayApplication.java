a0d8b43b37d9de57bf98e0cb2dc68df2d464eed3
huangwl <410488219@qq.com>
2020-06-29 18:27:47 +0800

    update deps(springboot 2.3.1 + jdk14 + maven reports error, so use gradle instead)

--- src/main/java/com/github/rainmanhhh/gateway/EurekaGatewayApplication.java
+++ src/main/java/com/github/rainmanhhh/gateway/EurekaGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EurekaGatewayApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(EurekaGatewayApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaGatewayApplication.class, args);
+	}
 
 }
