16ca99aa3052c96c2670e27d3f48ac4c6826c328
huangwl <410488219@qq.com>
2022-06-08 17:12:56 +0800

    update deps

--- src/main/kotlin/ez/gateway/eureka/EurekaGatewayApplication.kt
+++ src/main/kotlin/ez/gateway/eureka/EurekaGatewayApplication.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class EurekaGatewayApplication
 
-fun main() {
-  runApplication<EurekaGatewayApplication>()
+fun main(args: Array<String>) {
+  runApplication<EurekaGatewayApplication>(*args)
 }
